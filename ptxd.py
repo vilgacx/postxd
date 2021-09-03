6fde857fc33fbad06718287557a77533b7e0a0e8
CypherX7 <cypherx702@gmail.com>
2021-09-03 21:03:52 +0530

    nothing

--- ptxd.py
+++ ptxd.py
@@ -9,7 +9,7 @@ post_url=input('Post URL: ')
 filetype=str(sys.argv[2])
 
 reddit=requests.get('https://redditsave.com/info?url='+post_url)
-insta=requests.post('https://www.w3toys.com/',{'link':'https://www.instagram.com/p/CTMyzZILLwH/?utm_source=ig_web_copy_link','submit':'DOWNLOAD'})
+insta=requests.post('https://www.w3toys.com/',{'link':post_url,'submit':'DOWNLOAD'})
 
 #fb=requests.get()
 #twitter=requests.get()
